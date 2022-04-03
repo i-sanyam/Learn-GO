cdeb3b81628cc18644f2f74b8bf0077076c5777b
Sanyam Aggarwal <jsb@JSB-M1.local>
2022-04-03 12:39:16 +0530

    Different variable assignment ways

--- hello/main.go
+++ hello/main.go
@@ -5,10 +5,16 @@ import (
 	"net/http"
 )
 
+var card string
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path);
-		fmt.Println("Hi Gopher! %s", r.URL.Path);
+		card = "Ace of Spade"
+		card2 := "Ace of Diamond"
+		card2 = "Five of Club"
+		fmt.Println(card, card2)
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Println(r.URL.Path)
 	})
 
 	http.ListenAndServe(":3000", nil)
