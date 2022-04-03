8325da547224c3b49f91e1fe29eaf85f0451a1f8
Sanyam Aggarwal <i-sanyam@github.com>
2022-04-03 18:37:02 +0530

    read from file

--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,13 @@
 package main
 
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
 	// cards.print()
 	// hand, remainingCards := deal(cards, 5)
 	// hand.print()
 	// remainingCards.print()
 	// cards.print()
-	cards.saveToFile("cards1.txt")
-
+	// cards.saveToFile("cards1.txt")
+	cards := readDeckFromFile("cards1.txt")
+	cards.print()
 }
