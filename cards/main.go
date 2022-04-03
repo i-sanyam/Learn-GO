4687593f8c1e91a0bbe7d9e8e0210234cbb8c6f3
Sanyam Aggarwal <i-sanyam@github.com>
2022-04-03 17:48:00 +0530

    custom type conversion to string

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	cards.print()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	// cards.print()
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
+	// cards.print()
+	fmt.Println(cards.toString());
+
 }
