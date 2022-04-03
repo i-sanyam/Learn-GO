f3b43121c7573b500b925eda0dd4a0690d8bf306
Sanyam Aggarwal <i-sanyam@github.com>
2022-04-03 18:39:34 +0530

    reorganise cards code

--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,21 @@
 package main
 
 func main() {
+	// create a new deck and print it
 	// cards := newDeck()
 	// cards.print()
+
+
 	// hand, remainingCards := deal(cards, 5)
 	// hand.print()
 	// remainingCards.print()
 	// cards.print()
+
+	// save deck to a file
 	// cards.saveToFile("cards1.txt")
+
+	// read deck from a file
+	// cards := readDeckFromFile("cards1.txt") // test error
 	cards := readDeckFromFile("cards1.txt")
 	cards.print()
 }
