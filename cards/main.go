f086822ff881a1ae44f15fa2072f9eabb4ae41a2
Sanyam Aggarwal <i-sanyam@github.com>
2022-04-03 14:58:48 +0530

    create custom type for deck of cards

--- cards/main.go
+++ cards/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	cards := []string{newCard(), newCard(), "Six of Clubs"}
+	cards := deck{newCard(), newCard(), "Six of Clubs"}
 	cards = append(cards, "Two of Spades")
-	
+
 	for i, card := range cards {
-		fmt.Println(i, card);
+		fmt.Println(i, card)
 	}
 }
 
