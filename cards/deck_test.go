b5bf3901b93e94c0022b9f563e03136c7f5222ba
Sanyam Aggarwal <i-sanyam@github.com>
2022-04-04 09:41:04 +0530

    testing fn - assert elements in slice

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -8,4 +8,10 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 12 {
 		t.Errorf("Expected a deck length of 20, but got %v", len(d))
 	}
+	if d[0] != "Ace of Clubs" {
+		t.Errorf("Expected Ace of Clubs, but got %v", d[0])
+	}
+	if d[len(d)-1] != "Three of Spades" {
+		t.Errorf("Expected Three of Spades, but got %v", d[0])
+	}
 }
