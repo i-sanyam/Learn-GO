a06db030fbaebfd912f84df8cc1cafd3e9cb0ec3
Sanyam Aggarwal <i-sanyam@github.com>
2022-04-17 14:16:15 +0530

    another way to init map

--- maps/main.go
+++ maps/main.go
@@ -8,4 +8,20 @@ func main() {
 		"blue": "Le Mans Blue",
 	}
 	fmt.Println(colors);
+
+	// wrong way to initialise map
+	var bmws map[int]string
+	fmt.Println(bmws);
+	// bmws[1] = "7 Series" // throws error
+	// bmws[7] = "5 Series" // panic: assignment to entry in nil map
+
+	// correct way to initialise empty map
+	var mercs = make(map[string]int)
+	fmt.Println(mercs)
+	mercs["E Class"] = 211;
+	mercs["E Class Old"] = 210;
+	fmt.Println(mercs)
+	delete(mercs, "E Class Old");
+	fmt.Println(mercs)
+
 }
