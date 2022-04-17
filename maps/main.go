5e83402cf2af4c2996cdeea0fb750a9010a89fbc
Sanyam Aggarwal <i-sanyam@github.com>
2022-04-17 14:22:39 +0530

    iterate over maps

--- maps/main.go
+++ maps/main.go
@@ -4,24 +4,30 @@ import "fmt"
 
 func main() {
 	colors := map[string]string{
-		"red": "I am color red",
+		"red":  "I am color red",
 		"blue": "Le Mans Blue",
 	}
-	fmt.Println(colors);
+	fmt.Println(colors)
 
 	// wrong way to initialise map
 	var bmws map[int]string
-	fmt.Println(bmws);
+	fmt.Println(bmws)
 	// bmws[1] = "7 Series" // throws error
 	// bmws[7] = "5 Series" // panic: assignment to entry in nil map
 
 	// correct way to initialise empty map
 	var mercs = make(map[string]int)
 	fmt.Println(mercs)
-	mercs["E Class"] = 211;
-	mercs["E Class Old"] = 210;
+	mercs["E Class"] = 211
+	mercs["E Class Old"] = 210
+	mercs["S Class"] = 221
 	fmt.Println(mercs)
-	delete(mercs, "E Class Old");
-	fmt.Println(mercs)
+	delete(mercs, "E Class Old")
+	printCarsMap(mercs)
+}
 
+func printCarsMap(m map[string]int) {
+	for prettyName, codeName := range m {
+		fmt.Println("The code name of", prettyName, "is W", codeName)
+	}
 }
